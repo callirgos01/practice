47e8efc29d955a13fc4a335a9bc499d4a8fd52b1
Carlos Callirgos <callirgos01@gmail.com>
2018-03-10 16:27:49 -0800

    kthsmallest

--- src/net/callirgos/kthSmallest/KthSmallest.java
+++ src/net/callirgos/kthSmallest/KthSmallest.java
@@ -3,22 +3,63 @@ package net.callirgos.kthSmallest;
 import java.util.Arrays;
 
 public class KthSmallest {
-
-    public int findKthNumber(int m, int n, int k) {
+    /*
+    * Nearly every one have used the Multiplication Table. But could you find out the k-th smallest number quickly from
+    * the multiplication table?
+    * Given the height m and the length n of a m * n Multiplication Table, and a positive integer k, you need to return
+    * the k-th smallest number in this table.
+    * Example 1:
+    * Input:
+    *       m = 3, n = 3, k = 5
+    * Output:
+    *       5
+    * Explanation:
+    *   The Multiplication Table:
+    *  1	2    3
+    *  2	4	 6
+    *  3	6	 9
+    * The 5-th smallest number is 3 (1, 2, 2, 3, 3).
+    * Example 2:
+    * Input:
+    *       m = 2, n = 3, k = 6
+    * Output:
+    *       6
+    * Explanation:
+    *   The Multiplication Table:
+    *   1	 2	  3
+    *   2	 4	  6
+    * The 6-th smallest number is 6 (1, 2, 2, 3, 4, 6).
+    * Note:
+    *   The m and n will be in the range [1, 30000].
+    *   The k will be in the range [1, m * n]
+    * */
+   /* public int findKthNumber(int m, int n, int k) {
         int [] mTable = new int[m*n];
         for(int row=0; row<m; row++)
         {
             for(int col=0; col<n; col++)
             {
-                mTable[(row*m)+(col)] = (row+1)*(col+1);
+                mTable[(row*n)+(col)] = (row+1)*(col+1);
             }
         }
         Arrays.sort(mTable);
-        for( int num : mTable)
-        {
-            System.out.println(num);
-        }
         return mTable[k-1];
+    }*/
+    public boolean enough(int x, int m, int n, int k) {
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            count += Math.min(x / i, n);
+        }
+        return count >= k;
     }
 
+    public int findKthNumber(int m, int n, int k) {
+        int lo = 1, hi = m * n;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (!enough(mi, m, n, k)) lo = mi + 1;
+            else hi = mi;
+        }
+        return lo;
+    }
 }
--- tests/net/callirgos/kthSmallest/KthSmallestTest.java
+++ tests/net/callirgos/kthSmallest/KthSmallestTest.java
@@ -8,7 +8,36 @@ class KthSmallestTest {
 
     @Test
     void findKthNumber() {
+        int example1M = 3;
+        int example1N = 3;
+        int example1K = 5;
+        int expectedResult1 = 3;
 
+        int actualResult1 = new KthSmallest().findKthNumber(example1M, example1N, example1K);
 
+        int example2M = 2;
+        int example2N = 3;
+        int example2K = 6;
+        int expectedResult2 = 6;
+
+        int actualResult2 = new KthSmallest().findKthNumber(example2M, example2N, example2K);
+
+        int example3M = 3;
+        int example3N = 1;
+        int example3K = 3;
+        int expectedResult3 = 3;
+
+        int actualResult3 = new KthSmallest().findKthNumber(example3M, example3N, example3K);
+        int example4M = 9895;
+        int example4N = 28405;
+        int example4K = 100787757;
+        int expectedResult4 = 31666344;
+
+        int actualResult4 = new KthSmallest().findKthNumber(example4M, example4N, example4K);
+
+         assertEquals(expectedResult1, actualResult1);
+         assertEquals(expectedResult2, actualResult2);
+         assertEquals(expectedResult3, actualResult3);
+         assertEquals(expectedResult4, actualResult4);
     }
 }
